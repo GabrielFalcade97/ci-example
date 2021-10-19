2e5693e204df1603dd0cd1ad0a88666c99ec79c1
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-10-18 21:58:33 -0300

    corrigindo bugs e implementando os testes

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,14 +4,39 @@ describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
             expect(gerarNumeroAleatorio(20, -5))
-                .toBe(-5);
+                .toBe(-1);
         });
-    });
+
+        test('inicio tem que ser numero inteiro', () =>{
+            expect(gerarNumeroAleatorio(2.6,7))
+            .toBe(-1);
+        });
+
+        test('fim tem que ser um numero inteiro', () =>{
+            expect(gerarNumeroAleatorio(50, "notIntegerNumber"))
+            .toBe(-1);
+        });
+
+        test('inicio não deve ser negativo', ()=>{
+            expect(gerarNumeroAleatorio(-9, 8))
+            .toBe(-1);
+        });
+
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
+        });
+
+        test('Espera retornar indice cujo caracter foi encontrado', ()=>{
+            expect(acharCaracter(3, 'abc', 'b'))
+            .toBe(1);
+        });
+
+        test('Espera apresentar mensagem informando que caracter não é presente na cadeira', ()=>{
+            expect(acharCaracter(8, 'qwerty', 'a'))
+            .toBe("caracter não encontrado");
         });
     });    
 });
