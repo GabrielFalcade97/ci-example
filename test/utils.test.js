8721fac4400bad428fd9a56a350e3fe0950ac27a
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-10-18 22:08:38 -0300

    corrigindo teste

--- test/utils.test.js
+++ test/utils.test.js
@@ -12,32 +12,30 @@ describe('Utils', () => {
             .toBe(-1);
         });
 
-        test('fim tem que ser um numero inteiro', () =>{
-            expect(gerarNumeroAleatorio(50, "notIntegerNumber"))
+        test('fim tem que ser um numero inteiro',()=>{
+            expect(gerarNumeroAleatorio(50,"notInteger"))
             .toBe(-1);
         });
 
-        test('inicio não deve ser negativo', ()=>{
-            expect(gerarNumeroAleatorio(-9, 8))
+        test('inicio não deve ser negativo',()=>{
+            expect(gerarNumeroAleatorio(-9,8))
             .toBe(-1);
         });
     });
-
+    
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
-                .toBe('comprimento invalido');
+            .toBe('comprimento invalido');
         });
-
-        test('Espera retornar indice cujo caracter foi encontrado', ()=>{
-            expect(acharCaracter(3, 'abc', 'b'))
+        test('Espera retornar indice cujo caracter foi encontrado',()=>{
+            expect(acharCaracter(3,'abc','b'))
             .toBe(1);
-        });
-
-        test('Espera apresentar mensagem informando que caracter não é presente na cadeira', ()=>{
-            expect(acharCaracter(8, 'qwerty', 'a'))
-            .toBe("caracter não encontrado");
+        })
+        test('Espera apresentar mensagem informando que caracter não é presente na cadeira',()=>{
+        expect(acharCaracter(5,'qwert','a'))
+        .toBe("caracter não encontrado");
         });
     });    
 });
